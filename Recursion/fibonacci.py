8a6a661837b436e1b3e0a82041097d78920d7b82
Yash Narnaware <70089229+Yash-Narnaware@users.noreply.github.com>
2025-06-09 17:35:46 +0530

    Create fibonacci.py

---
+++ Recursion/fibonacci.py
@@ -0,0 +1,8 @@
+# A function gives n+1th fibonacci number as python is 0 indexed language.
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(3))
