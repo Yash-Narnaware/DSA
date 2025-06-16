7147c9f742fef64d795ed6b3eecfc5497680e25f
Yash Narnaware <70089229+Yash-Narnaware@users.noreply.github.com>
2025-06-16 23:30:50 +0530

    Create 21.Partitions_with_Given_Difference.py

---
+++ Dynamic Programming/21.Partitions_with_Given_Difference.py
@@ -0,0 +1,59 @@
+#GFG - https://www.geeksforgeeks.org/problems/partitions-with-given-difference/1
+
+#partition array s.t. difference of sum of element in this 2 partitions is equal to d.
+
+
+#Same as #20 just little change in base condition and return statement
+
+
+
+#Recursive approach
+
+from typing import List
+class Solution:
+    def countPartitions(self, arr, d):
+
+        sum2 = sum(arr)
+        
+        def func(sum1,n):
+            
+            if n == 0:
+                if (2*sum1 - sum2) == d:
+                    return 1
+                else:
+                    return 0
+                    
+            return func(sum1 + arr[n-1],n-1) + func(sum1,n-1)
+            
+        return func(0,len(arr))
+        
+
+
+
+#Memoization - Top down
+
+from typing import List
+class Solution:
+    def countPartitions(self, arr, d):
+
+        sum2 = sum(arr)
+        mem = {}
+        def func(sum1,n):
+            
+            if (sum1,n) not in mem:
+                if n == 0:
+                    if (2*sum1 - sum2) == d:
+                        mem[(sum1,n)] = 1
+                    else:
+                        mem[(sum1,n)] = 0
+                else:        
+                    mem[(sum1,n)] = func(sum1 + arr[n-1],n-1) + func(sum1,n-1)
+            
+            return mem[(sum1,n)]
+            
+        return func(0,len(arr))
+        
+
+
+#Tabulation - Bottom up
+#TBD
