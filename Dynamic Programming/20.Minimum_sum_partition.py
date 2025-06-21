b5a4cd9f79bbf9b190be19a3f4f621b404eb77ad
Yash Narnaware <70089229+Yash-Narnaware@users.noreply.github.com>
2025-06-21 22:16:00 +0530

    Update 20.Minimum_sum_partition.py

--- Dynamic Programming/20.Minimum_sum_partition.py
+++ Dynamic Programming/20.Minimum_sum_partition.py
@@ -44,4 +44,32 @@ class Solution:
 
 
 #Tabulation - bottom up
-#TBD
+
+#Find if sum of subsets from 0 to sum(arr) is possible or not - use tabulation store true/False values for sum and n
+#then iterate through last row of dp table and if that sum is possible using that sum compute other partition sum and find the minimum abs difference
+
+
+class Solution:
+	def minDifference(self, arr):
+
+
+        n = len(arr)
+        m = sum(arr)
+        
+        dp = [[False]*(m+1) for _ in range(n+1)]
+        
+        for i in range(n+1):
+            dp[i][0] = True
+            
+        
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                dp[i][j] = dp[i-1][j] or dp[i-1][j - arr[i-1]]
+                
+        
+        res = float('inf')
+        for i,j in enumerate(dp[n]):
+            if j == True: 
+                res = min(res, abs(m - 2*i))
+        
+        return res
