5a9f53e289949ed8e82a00c6657b150edef56293
Yash Narnaware <70089229+Yash-Narnaware@users.noreply.github.com>
2025-07-04 19:28:25 +0530

    Create 10. Find Peak Element.py

---
+++ Binary Search/10. Find Peak Element.py
@@ -0,0 +1,25 @@
+#leetcode - https://leetcode.com/problems/find-peak-element/description/
+
+
+#GO in the direction of higher element.
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+
+
+        l = 0
+        r = len(nums) - 1
+
+        while l < r:
+
+            mid = (l + r) // 2
+
+            if nums[mid - 1] < nums[mid] > nums[mid + 1]:
+                return mid
+            if nums[mid] < nums[mid + 1]:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return l
+        
