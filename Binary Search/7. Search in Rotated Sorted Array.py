3ddf8d163286840186e1f603302389ca0031c5ff
Yash Narnaware <70089229+Yash-Narnaware@users.noreply.github.com>
2025-07-09 01:05:53 +0530

    Update 7. Search in Rotated Sorted Array.py

--- Binary Search/7. Search in Rotated Sorted Array.py
+++ Binary Search/7. Search in Rotated Sorted Array.py
@@ -2,6 +2,8 @@
 
 #First find the rotated index then deter the boundries of search according to target value and rotated index then find the target.
 
+#Better approach would be calculate mid if target == nums[mid] return mid then determine the sorted half using mid, l and r values. check if target is between the sorted array and change l and r bounds according to it.
+
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
@@ -45,6 +47,32 @@ class Solution:
                 r = mid - 1
             else:
                 l = mid + 1
+
+#Better approach
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        n = len(nums)
+        l = 0
+        r = n - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[l]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+        return -1
+
 
         return -1
         
