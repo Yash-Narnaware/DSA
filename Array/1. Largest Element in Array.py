b40d4b9b6b37ac0fb3df150fe3d4e2e469405cc3
Yash Narnaware <70089229+Yash-Narnaware@users.noreply.github.com>
2025-07-16 00:08:51 +0530

    Create 1. Largest Element in Array.py

---
+++ Array/1. Largest Element in Array.py
@@ -0,0 +1,9 @@
+#GFG - https://www.geeksforgeeks.org/problems/largest-element-in-array4009/1
+
+class Solution:
+    def largest(self, arr):
+
+        res = arr[0]
+        for i in range(1, len(arr)):
+            res = max(res, arr[i])
+        return res
