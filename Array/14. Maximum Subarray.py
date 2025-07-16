3d88dbff9ee7a6181dae113ea3ebaaeb53be3b40
Yash Narnaware <70089229+Yash-Narnaware@users.noreply.github.com>
2025-07-16 23:47:39 +0530

    Update 14. Maximum Subarray.py

--- Array/14. Maximum Subarray.py
+++ Array/14. Maximum Subarray.py
@@ -17,4 +17,32 @@ class Solution:
 
         return res
 
+
+#Printing maximum subarray - just keep track of start and end inex of max sum subarray using 2 variables and only update them when you found new max sum
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        sum1 = 0
+        res = float('-inf')
+        mx_start, mx_end = 0,0
+        temp_start = 0
+
+        for i in range(len(nums)):
+            sum1 += nums[i]
+            if sum1 > res:
+                mx_end = i
+                mx_start = temp_start
+                res = sum1
+
+            if sum1 < 0:
+                temp_start = i+1
+                sum1 = 0
+        
+        print(nums[mx_start:mx_end+1])
+
+        return res
+
+        
+
+
         
