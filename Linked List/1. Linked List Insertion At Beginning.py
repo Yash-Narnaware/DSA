74a3b1eb931dde4a0e6ec2c2a1cf046262debfa5
Yash Narnaware <70089229+Yash-Narnaware@users.noreply.github.com>
2025-07-20 21:35:47 +0530

    Create 1. Linked List Insertion At Beginning.py

---
+++ Linked List/1. Linked List Insertion At Beginning.py
@@ -0,0 +1,43 @@
+#Leetcode - https://leetcode.com/problems/delete-node-in-a-linked-list/description/
+
+
+
+#My approach - 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        cur = node
+        prev = None
+
+        while cur.next:
+            cur.val = cur.next.val
+            prev = cur
+            cur = cur.next
+        prev.next = None
+
+
+#Ever clever approach that I knew but forgot - 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+        
