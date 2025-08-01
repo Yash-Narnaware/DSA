e826f9953375d6b692a089aec3cba0294c30b161
Yash Narnaware <70089229+Yash-Narnaware@users.noreply.github.com>
2025-08-01 23:29:40 +0530

    Create 4. Sum of Subarray Minimums.py

---
+++ Stack and Queues/4. Sum of Subarray Minimums.py
@@ -0,0 +1,46 @@
+#Leetcode - https://leetcode.com/problems/sum-of-subarray-minimums/description/
+
+#calculate next smaller element indices and previous smaller element indices and calculate the subarrays with ith element as minimum and add them up. 
+#For ith element the number of subarrays in which theat ith element is minimum is number of elements between pse * number of elements between nse((i - pse[i])*(nse[i] - i))
+
+from collections import deque
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+
+        MOD = 10**9 + 7
+        n = len(arr)
+        nse = [n]*n
+
+        #Calculating the next smaller element index
+        stack = deque([])
+        for i in range(n-1, -1, -1):
+            while stack and arr[i] <= stack[-1][0]:
+                stack.pop()
+
+            if stack:
+                nse[i] = stack[-1][1]
+            stack.append((arr[i], i))
+
+        pse = [-1]*n
+        #Calculating the previous smaller element index
+        stack = deque([])
+        for i in range(n):
+            while stack and arr[i] < stack[-1][0]:
+                stack.pop()
+
+            if stack:
+                pse[i] = stack[-1][1]
+            stack.append((arr[i], i))
+
+        #calculating number of subarrays with ith element as minimum
+        res = 0
+        for i in range(n):
+            left = i - pse[i]
+            right = nse[i] - i
+
+            res += (left*right*arr[i])
+
+        return res % MOD
+
+
+        
