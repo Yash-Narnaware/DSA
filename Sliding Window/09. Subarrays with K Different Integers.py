ee57b5b35cfb6dd94876d7bb164b33e5675cb38d
Yash Narnaware <70089229+Yash-Narnaware@users.noreply.github.com>
2025-09-03 22:13:22 +0530

    Create 09. Subarrays with K Different Integers.py

---
+++ Sliding Window/09. Subarrays with K Different Integers.py
@@ -0,0 +1,34 @@
+#Leetcode - https://leetcode.com/problems/subarrays-with-k-different-integers/description/
+
+#got TLE using brute force. then applied logic of exactly k distic element sub array = atmost k distinct subarrays - atmost k-1 distinct subarrays.
+
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+      
+        def func(k):
+            freq = {}
+            start = 0
+            end = 0
+            res = 0
+
+            while end < len(nums):
+
+                if nums[end] in freq:
+                    freq[nums[end]] += 1
+                else:
+                    freq[nums[end]] = 1
+
+                while len(freq) > k:
+                    freq[nums[start]] -= 1
+                    if freq[nums[start]] == 0:
+                        del freq[nums[start]]
+                    start += 1
+                
+                res += end - start + 1
+
+                end += 1
+
+            return res
+
+        return func(k) - func(k - 1)
+        
