d63bfae4454418f5fe4c17625ce9d9d8ffbd7872
Yash Narnaware <70089229+Yash-Narnaware@users.noreply.github.com>
2025-09-04 18:18:19 +0530

    Update 09. Subarrays with K Different Integers.py

--- Sliding Window/09. Subarrays with K Different Integers.py
+++ Sliding Window/09. Subarrays with K Different Integers.py
@@ -1,6 +1,6 @@
 #Leetcode - https://leetcode.com/problems/subarrays-with-k-different-integers/description/
 
-#got TLE using brute force. then applied logic of exactly k distic element sub array = atmost k distinct subarrays - atmost k-1 distinct subarrays.
+#got TLE using brute force. then applied logic of exactly k distinct element sub array = atmost k distinct subarrays - atmost k-1 distinct subarrays.
 
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
@@ -24,6 +24,7 @@ class Solution:
                         del freq[nums[start]]
                     start += 1
                 
+                #Number of sub-arrays ending with nums[end]
                 res += end - start + 1
 
                 end += 1
